c18dd45c81aabe1f0e6d2f879396dd71d65dab24
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-06-22 13:18:59 +0200

    the details of second task

---
+++ zadanie2/README.md
@@ -0,0 +1,15 @@
+### Task 2 
+if I manage to do it I'll receive better grade<br>
+soo...
+
+dy / dt = (1-2y)y^2  
+
+y(0) = 1
+
+t ∈ 〈0,5〉 
+
+solve using Rungego-Kutty method (4 degree)
+
+Present the solution graphically with the integration step h = 0.5
+
+Next do the same using different integration steps
---
+++ zadanie2/main.cpp
@@ -0,0 +1,73 @@
+//
+// Created by studio25 on 22.06.2021.
+//
+#include <array>
+#include <iostream>
+#include <vector>
+
+template <size_t H, size_t W> class Mat {
+public:
+  Mat() {
+    for (auto &i : data_)
+      i.fill(0.0);
+  }
+
+  Mat(const Mat<W, H> &other) {
+    unsigned height = w_ < other.w_ ? w_ : other.w_;
+    unsigned width = w_ < other.w_ ? w_ : other.w_;
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        data_[i][j] = other.data_[i][j];
+      }
+    }
+  }
+
+  Mat &operator=(const Mat<H, W> &other) {
+    if (this == &other)
+      return *this;
+    data_ = other.data_;
+    return *this;
+  }
+
+  void FromVec(std::vector<std::vector<double>> in_data) {
+    for (size_t x = 0; x < h_; x++) {
+      for (size_t y = 0; y < w_; y++)
+        data_[x][y] = in_data[x][y];
+    }
+  }
+
+  template <size_t N, size_t M> void FromMat(const Mat<N, M> &other) {
+    unsigned height = w_ < other.w_ ? w_ : other.w_;
+    unsigned width = w_ < other.w_ ? w_ : other.w_;
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        data_[i][j] = other.data_[i][j];
+      }
+    }
+  }
+
+  double &At(size_t x, size_t y) { return data_[x][y]; }
+
+  friend std::ostream &operator<<(std::ostream &out, const Mat<H, W> &ref) {
+    for (size_t x = 0; x < ref.h_; x++) {
+      for (size_t y = 0; y < ref.w_; y++)
+        out << ref.data_[x][y] << '\t';
+      out << '\n';
+    }
+    return out;
+  }
+
+  const size_t w_ = W;
+  const size_t h_ = H;
+
+  std::array<std::array<double, W>, H> data_;
+
+private:
+};
+
+int main(){
+
+
+}
