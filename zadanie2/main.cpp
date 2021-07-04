dbc891488c1dbebb1f0acf548f026c5696822e20
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-05 00:07:51 +0200

    Rk4 is copied from wiki

--- zadanie2/main.cpp
+++ zadanie2/main.cpp
@@ -5,71 +5,33 @@
 #include <iostream>
 #include <vector>
 
-template <size_t H, size_t W> class Mat {
-public:
-  Mat() {
-    for (auto &i : data_)
-      i.fill(0.0);
+#define H 0.5
+#define PI 3.141592653589793238463
+double Function(double x, double y) { return (1 - (2 * y)) * y * y; }
+double Rk4(double xn, double yn);
+int main() {
+
+
+   // y(0) = 1
+
+   double y0 = 1;
+
+
+  std::cout<<0<<"\t"<<y0<<"\n";
+  for (double i = H; i < 5; i += H) {
+    y0 += Rk4(0,i);
+    std::cout<<i<<"\t"<<y0<<"\n";
   }
 
-  Mat(const Mat<W, H> &other) {
-    unsigned height = w_ < other.w_ ? w_ : other.w_;
-    unsigned width = w_ < other.w_ ? w_ : other.w_;
+  return 0;
+}
 
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        data_[i][j] = other.data_[i][j];
-      }
-    }
-  }
+double Rk4(double xn, double yn) {
 
-  Mat &operator=(const Mat<H, W> &other) {
-    if (this == &other)
-      return *this;
-    data_ = other.data_;
-    return *this;
-  }
+  double k_1 = H * Function(xn, yn);
+  double k_2 = H * Function(xn + H / 2, yn + k_1 / 2);
+  double k_3 = H * Function(xn + H / 2, yn + k_2 / 2);
+  double k_4 = H * Function(xn + H, yn + k_3);
 
-  void FromVec(std::vector<std::vector<double>> in_data) {
-    for (size_t x = 0; x < h_; x++) {
-      for (size_t y = 0; y < w_; y++)
-        data_[x][y] = in_data[x][y];
-    }
-  }
-
-  template <size_t N, size_t M> void FromMat(const Mat<N, M> &other) {
-    unsigned height = w_ < other.w_ ? w_ : other.w_;
-    unsigned width = w_ < other.w_ ? w_ : other.w_;
-
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        data_[i][j] = other.data_[i][j];
-      }
-    }
-  }
-
-  double &At(size_t x, size_t y) { return data_[x][y]; }
-
-  friend std::ostream &operator<<(std::ostream &out, const Mat<H, W> &ref) {
-    for (size_t x = 0; x < ref.h_; x++) {
-      for (size_t y = 0; y < ref.w_; y++)
-        out << ref.data_[x][y] << '\t';
-      out << '\n';
-    }
-    return out;
-  }
-
-  const size_t w_ = W;
-  const size_t h_ = H;
-
-  std::array<std::array<double, W>, H> data_;
-
-private:
-};
-
-
-int main(){
-
-
-return 0;
+  return (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6;
 }
--- zadanie2/main_zuza.cpp
+++ zadanie2/main_zuza.cpp
@@ -1,77 +1,81 @@
 #include <iostream>
-double f(double x, double y)
-{
-    return 1.0 + y/x;
-}
-
+double f(double x, double y) { return 1.0 + y / x; }
 
 int main() {
-    const int n = 100;
-    //PRZEDZIAL
+  const int n = 100;
+  // PRZEDZIAL
 
-    double a = 1;
-    double b = 5;
+  double a = 1;
+  double b = 5;
 
-    double h = (b-a) / n;
+  double h = (b - a) / n;
 
-    double x[n+1];
-    double y[n+1];
-    double k[4];
-    x[0] = a;
-    x[n] = b;
-   // x[n-1] = b;
-    y[0] = 1;
-    for(int i = 0; i<n-1; i++)
-    {
-        x[i+1] = a + (i+1) * h;
-    }
-    std::cout<<"Pierwsza metoda:"<<std::endl;
-    std::cout<<"h: "<<h<<std::endl;
-    for(int i=0; i<n; i++)
-    {
-        k[0] = f(x[i], y[i]);
-        k[1] = f(x[i] + h/2, y[i] +  k[0]/2 * h);
-        k[2] = f(x[i] + h/2, y[i] + k[1]/2 * h);
-        k[3] = f(x[i] + h, y[i] +  k[2] * h);
-      //  std::cout<<"k[0] "<<k[0]<<" k[1]: "<<k[1]<<" k[2]: "<<k[2]<<" k[3]: "<<k[3]<<std::endl;
-        std::cout<<"x: "<<x[i]<<" y: "<<y[i]<<std::endl;
-        y[i+1] = 1.0 * y[i] + ( h  *(k[0] + 2.0*k[1] + 2.0 *k[2] + k[3])/6);
-    }
-    std::cout<<"x: "<<x[n]<<" y: "<<y[n]<<std::endl;
-    std::cout<<"-------------------------------------------------------------------------------------"<<std::endl;
-    std::cout<<"Druga metoda rzedu piatego:"<<std::endl;
-    double y2[n+1];
-    y2[0] = y[0];
-    y2[n] = y[n];
-    double j[5];
-    for(int i=0; i<n; i++)
-    {
-        j[0] = f(x[i], y2[i]);
-        j[1] = f(x[i] + h/4.0, y2[i] +  j[0]/4.0 * h);
-        j[2] = f(x[i] + 3.0* h/8.0, y2[i] + 3.0* j[1]/8.0 * h);
-        j[3] = f(x[i] + 12.0 * h/13.0, y2[i] +  12.0*j[2]/13.0 * h);
-        j[4] = f(x[i] +  1.0*h, y2[i] +  1.0*j[3]* h);
+  double x[n + 1];
+  double y[n + 1];
+  double k[4];
+  x[0] = a;
+  x[n] = b;
+  // x[n-1] = b;
+  y[0] = 1;
+  for (int i = 0; i < n - 1; i++) {
+    x[i + 1] = a + (i + 1) * h;
+  }
+  std::cout << "Pierwsza metoda:" << std::endl;
+  std::cout << "h: " << h << std::endl;
+  for (int i = 0; i < n; i++) {
+    k[0] = f(x[i], y[i]);
+    k[1] = f(x[i] + h / 2, y[i] + k[0] / 2 * h);
+    k[2] = f(x[i] + h / 2, y[i] + k[1] / 2 * h);
+    k[3] = f(x[i] + h, y[i] + k[2] * h);
+    //  std::cout<<"k[0] "<<k[0]<<" k[1]: "<<k[1]<<" k[2]: "<<k[2]<<" k[3]:
+    //  "<<k[3]<<std::endl;
+    std::cout << "x: " << x[i] << " y: " << y[i] << std::endl;
+    y[i + 1] = 1.0 * y[i] + (h * (k[0] + 2.0 * k[1] + 2.0 * k[2] + k[3]) / 6);
+  }
+  std::cout << "x: " << x[n] << " y: " << y[n] << std::endl;
+  std::cout << "---------------------------------------------------------------"
+               "----------------------"
+            << std::endl;
+  std::cout << "Druga metoda rzedu piatego:" << std::endl;
+  double y2[n + 1];
+  y2[0] = y[0];
+  y2[n] = y[n];
+  double j[5];
+  for (int i = 0; i < n; i++) {
+    j[0] = f(x[i], y2[i]);
+    j[1] = f(x[i] + h / 4.0, y2[i] + j[0] / 4.0 * h);
+    j[2] = f(x[i] + 3.0 * h / 8.0, y2[i] + 3.0 * j[1] / 8.0 * h);
+    j[3] = f(x[i] + 12.0 * h / 13.0, y2[i] + 12.0 * j[2] / 13.0 * h);
+    j[4] = f(x[i] + 1.0 * h, y2[i] + 1.0 * j[3] * h);
 
-        //  std::cout<<"k[0] "<<k[0]<<" k[1]: "<<k[1]<<" k[2]: "<<k[2]<<" k[3]: "<<k[3]<<std::endl;
-        std::cout<<"x: "<<x[i]<<" y: "<<y2[i]<<std::endl;
-        y2[i+1] = 1.0 * y2[i] + ((25*j[0]/216 + 1408*j[2]/2565 + 2197 *j[3]/4104 - j[4]/5) * h);
-    }
-    std::cout<<"x: "<<x[n]<<" y: "<<y2[n]<<std::endl;
-    std::cout<<"-------------------------------------------------------------------------------------"<<std::endl;
-    std::cout<<"Druga metoda rzedu szóstego:"<<std::endl;
-    y2[0] = 1;
-    double l[6];
-    for(int i=0; i<n; i++)
-    {
-        l[0] = f(x[i], y2[i]);
-        l[1] = f(x[i] + h/4.0, y2[i] +  j[0]/4.0 * h);
-        l[2] = f(x[i] + 3.0* h/8.0, y2[i] + 3* j[1]/8.0 * h);
-        l[3] = f(x[i] + 12.0 * h/13.0, y2[i] +  12.0*j[2]/13.0 * h);
-        l[4] = f(x[i] +  1.0*h, y2[i] +  1.0*j[3]* h);
-        l[5] = f(x[i] + h/2.0, y2[i] +  j[4] * h/2.0);
-        std::cout<<"x: "<<x[i]<<" y: "<<y[i]<<std::endl;
-        y2[i+1] = 1.0 * y2[i] + ((16.0*l[0]/135.0) + (6656.0*l[2]/12825.0) + (28561.0 *l[3]/56430.0) - (9.0*l[4]/5.0) + (2.0*l[5]/55.0)) * h;
-    }
-    std::cout<<"x: "<<x[n]<<" y: "<<y[n]<<std::endl;
-    return 0;
+    //  std::cout<<"k[0] "<<k[0]<<" k[1]: "<<k[1]<<" k[2]: "<<k[2]<<" k[3]:
+    //  "<<k[3]<<std::endl;
+    std::cout << "x: " << x[i] << " y: " << y2[i] << std::endl;
+    y2[i + 1] = 1.0 * y2[i] + ((25 * j[0] / 216 + 1408 * j[2] / 2565 +
+                                2197 * j[3] / 4104 - j[4] / 5) *
+                               h);
+  }
+  std::cout << "x: " << x[n] << " y: " << y2[n] << std::endl;
+  std::cout << "---------------------------------------------------------------"
+               "----------------------"
+            << std::endl;
+  std::cout << "Druga metoda rzedu szóstego:" << std::endl;
+  y2[0] = 1;
+  double l[6];
+  for (int i = 0; i < n; i++) {
+    l[0] = f(x[i], y2[i]);
+    l[1] = f(x[i] + h / 4.0, y2[i] + j[0] / 4.0 * h);
+    l[2] = f(x[i] + 3.0 * h / 8.0, y2[i] + 3 * j[1] / 8.0 * h);
+    l[3] = f(x[i] + 12.0 * h / 13.0, y2[i] + 12.0 * j[2] / 13.0 * h);
+    l[4] = f(x[i] + 1.0 * h, y2[i] + 1.0 * j[3] * h);
+    l[5] = f(x[i] + h / 2.0, y2[i] + j[4] * h / 2.0);
+    std::cout << "x: " << x[i] << " y: " << y[i] << std::endl;
+    y2[i + 1] =
+        1.0 * y2[i] + ((16.0 * l[0] / 135.0) + (6656.0 * l[2] / 12825.0) +
+                       (28561.0 * l[3] / 56430.0) - (9.0 * l[4] / 5.0) +
+                       (2.0 * l[5] / 55.0)) *
+                          h;
+  }
+  std::cout << "x: " << x[n] << " y: " << y[n] << std::endl;
+  return 0;
 }
