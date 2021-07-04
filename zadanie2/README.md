0d8bc70ead88dbdee9dbdccda672b4e8e2332f46
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-05 00:29:44 +0200

    readme update

--- zadanie2/README.md
+++ zadanie2/README.md
@@ -19,3 +19,9 @@ Full task can be found [here](Projekt2-MetNum.pdf) (I have to do the second one)
 The [main_zuza.cpp](main_zuza.cpp) is complete and working answer to my task.
 
 I have collected some 
+
+
+
+```RK4 method {y'[x] = (1 − 2*x)*x*x , y[0] == 1} from 0 to 5 stepsize = 0.5```
+
+in [wolfram](https://www.wolframalpha.com/input/?i=RK4+method+%7By%27%5Bx%5D+%3D+%281+%E2%88%92+2*x%29*x*x+%2C+y%5B0%5D+%3D%3D+1%7D+from+0+to+5+stepsize+%3D+0.5)
--- zadanie2/main.cpp
+++ zadanie2/main.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 
 #define H 0.5
-#define PI 3.141592653589793238463
 double Function(double x, double y) { return (1 - (2 * y)) * y * y; }
 double Rk4(double xn, double yn);
 int main() {
@@ -16,9 +15,8 @@ int main() {
 
    double y0 = 1;
 
-
   std::cout<<0<<"\t"<<y0<<"\n";
-  for (double i = H; i < 5; i += H) {
+  for (double i = H; i <= 5; i += H) {
     y0 += Rk4(0,i);
     std::cout<<i<<"\t"<<y0<<"\n";
   }
@@ -29,9 +27,9 @@ int main() {
 double Rk4(double xn, double yn) {
 
   double k_1 = H * Function(xn, yn);
-  double k_2 = H * Function(xn + H / 2, yn + k_1 / 2);
-  double k_3 = H * Function(xn + H / 2, yn + k_2 / 2);
+  double k_2 = H * Function(xn + (H / 2.0), yn + (k_1 / 2.0));
+  double k_3 = H * Function(xn + (H / 2.0), yn + (k_2 / 2.0));
   double k_4 = H * Function(xn + H, yn + k_3);
 
-  return (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6;
+  return (k_1 + (2.0 * k_2) + (2.0 * k_3) + k_4) / 6.0;
 }
