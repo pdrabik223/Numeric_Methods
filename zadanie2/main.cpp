77425a6d9ee23e5e3bebe08b0e09541d0a4b7cdd
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-06 13:05:37 +0200

    it works, I mean it generates data

--- zadanie2/main.cpp
+++ zadanie2/main.cpp
@@ -7,29 +7,41 @@
 
 #define H 0.5
 double Function(double x, double y) { return (1 - (2 * y)) * y * y; }
+
 double Rk4(double xn, double yn);
 int main() {
 
+  const double kH = 0.5; // step
+  const double kA = 0;   // t ∈ 〈0,5〉  so a is the leftmost value
+  const double kB = 10;   // b is end of a range
 
-   // y(0) = 1
+  const int kN = (kB - kA) / kH; // the number of slices
 
-   double y0 = 1;
+  double x[kN + 1];
+  double y[kN + 1];
 
-  std::cout<<0<<"\t"<<y0<<"\n";
-  for (double i = H; i <= 5; i += H) {
-    y0 += Rk4(0,i);
-    std::cout<<i<<"\t"<<y0<<"\n";
-  }
+  x[0] = kA;
+  for (int i = 1; i < kN; i++)
+    x[i] = kA + i * kH;
+
+  x[kN] = kB;
+
+  y[0] = 1;
+  for (int i = 0; i < kN; i++)
+    y[i + 1] = y[i] + Rk4(x[i], y[i]);
+
+  for (int i = 0; i <= kN; i++)
+    std::cout << x[i] << "\t" << y[i] << "\n";
 
   return 0;
 }
 
 double Rk4(double xn, double yn) {
 
-  double k_1 = H * Function(xn, yn);
-  double k_2 = H * Function(xn + (H / 2.0), yn + (k_1 / 2.0));
-  double k_3 = H * Function(xn + (H / 2.0), yn + (k_2 / 2.0));
-  double k_4 = H * Function(xn + H, yn + k_3);
+  double k_1 = Function(xn, yn);
+  double k_2 = Function(xn + (H / 2.0), yn + (k_1 / 2.0) * H);
+  double k_3 = Function(xn + (H / 2.0), yn + (k_2 / 2.0) * H);
+  double k_4 = Function(xn + H, yn + k_3 * H);
 
-  return (k_1 + (2.0 * k_2) + (2.0 * k_3) + k_4) / 6.0;
+  return H * (k_1 + (2.0 * k_2) + (2.0 * k_3) + k_4) / 6.0;
 }
